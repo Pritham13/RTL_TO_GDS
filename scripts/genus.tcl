cab70a7322e8bd1d9e7e786650a7025a5e02d14a
Pritham13 <prithamhsuvarna@gmail.com>
2024-09-01 23:19:46 +0530

    updating genus script

--- scripts/genus.tcl
+++ scripts/genus.tcl
@@ -1,6 +1,6 @@
-set_attribute init_hdl_search_path rtl/
-set_attribute init_lib_search_path sky130hd/
-set_attr library sky130hd/sky130_fd_sc_hd__tt_025C_1v80.lib
+set_attribute init_hdl_search_path ../rtl/
+set_attribute init_lib_search_path ../sky130hd/
+set_attr library ../sky130hd/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_hdl {mod_N_counter_FSM_2.v}
 read_sdc constraints/constraints.sdc 
 elaborate
@@ -8,4 +8,4 @@ synthesize -to_mapped -effort high
 
 report area > area.rpt 
 report timing > timing.rpt 
-write_hdl  GLS_netlist.v
+write_hdl > GLS_netlist.v
