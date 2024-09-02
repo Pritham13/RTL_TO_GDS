73e3f0709f213669f1749401592006316abe5f50
zero <prithamhsuvarna@gmail.com>
2024-09-02 23:51:13 +0530

    updating tcl script

--- scripts/genus.tcl
+++ scripts/genus.tcl
@@ -1,6 +1,7 @@
 set_attribute init_hdl_search_path rtl/
 set_attribute init_lib_search_path libs/
-set_attr library libs/slow.lib
+#set_attr library libs/slow.lib
+set_attr library sky130hd/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_hdl {mod_N_counter_FSM_2.v}
 elaborate
 read_sdc constraints/constraints.sdc 
