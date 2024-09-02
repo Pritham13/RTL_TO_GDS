ab3fa27f2b6654ced6742c6504b183c37f42b882
zero <prithamhsuvarna@gmail.com>
2024-09-02 21:10:08 +0530

    uploading scripts

--- scripts/innovus.tcl
+++ scripts/innovus.tcl
@@ -1,8 +1,10 @@
 set init_gnd_net VSS
-set init_lef_file {/opt/PDKs/digital/45nm/dig/lef/gsclib045_tech.lef}
+#set init_lef_file {/opt/PDKs/digital/45nm/dig/lef/gsclib045_tech.lef}
+set init_lef_file sky130hd/sky130tech.lef 
+loadLefFile sky130hd/sky130hd_std_cell.lef
 set init_verilog netlists/genus_netlist.v
 set init_mmmc_file MMMC_files/Default.view
 set init_pwr_net VDD
 init_design
 
-floorPlan -coreMarginsBy die -site CoreSite -r 0.999758842733 0.7 260 260 260 260
+#floorPlan -coreMarginsBy die -site CoreSite -r 0.999758842733 0.7 260 260 260 260
